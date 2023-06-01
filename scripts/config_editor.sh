8295b63cac93988480ad5a45a6bda87179cfcb64
Adeyomola Kazeem <44479277+Adeyomola@users.noreply.github.com>
2023-06-01 17:27:55 +0100

    Update config_editor.sh

--- scripts/config_editor.sh
+++ scripts/config_editor.sh
@@ -2,6 +2,6 @@
 
 mv wp-config-sample.php wp-config.php
 
-sed -i "s|database_name_here.\+|$DB_DATABASE|" wp-config.php
-sed -i "s|username_here'.\+|$DB_USERNAME|" wp-config.php
-sed -i "s|password_here.\+|$DB_PASSWORD|" wp-config.php
+sed -i "s|database_name_here|$DB_DATABASE|" wp-config.php
+sed -i "s|username_here|$DB_USERNAME|" wp-config.php
+sed -i "s|password_here|$DB_PASSWORD|" wp-config.php
